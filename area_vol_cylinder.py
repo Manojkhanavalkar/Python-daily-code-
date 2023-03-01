bcba483e1f0974441439ffe8533d2f700c8c05f6
Manoj <khanavalkarmanoj@gmail.com>
2023-03-01 09:18:23 +0530

    area and vol of cylinder

---
+++ area_vol_cylinder.py
@@ -0,0 +1,7 @@
+radius=int(input("Enter the radius of cylinder: "))
+height=int(input("enter the height of cylinder:"))
+PI=3.14
+volume=(PI*radius*radius*height)
+Area=(2*PI*radius*height)+(2*PI*radius*radius)
+print("volume=",volume)
+print("area=",Area)
